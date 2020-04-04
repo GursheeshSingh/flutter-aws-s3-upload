8549a2cb33e80f61eed9f8a73318239250224fb8
Gursheesh Singh <singhgursheesh12@gmail.com>
2020-04-05 01:41:48 +0530

    Initial commit

---
+++ config-aws.js
@@ -0,0 +1,4 @@
+module.exports.accessKeyId = "yourAccessKey"; //TODO: Update
+module.exports.secretAccessKey = "yourSecretKey"; //TODO: Update
+module.exports.bucketName = "bucketName"; //TODO: Update
+module.exports.region = "bucketRegion"; //TODO: Update
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const AWS = require("aws-sdk");
+const awsConfig = require("../aws-server/config-aws");
+const uuid = require("uuid");
+const app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
+
+AWS.config.update({ region: awsConfig.region });
+
+const S3_BUCKET = awsConfig.bucketName;
+const s3 = new AWS.S3({
+  accessKeyId: awsConfig.accessKeyId,
+  secretAccessKey: awsConfig.secretAccessKey,
+  region: awsConfig.region,
+  signatureVersion: "v4",
+  //   useAccelerateEndpoint: true
+});
+
+const getPresignedUrl = (req, res) => {
+  let fileType = req.body.fileType;
+  if (fileType != ".jpg" && fileType != ".png" && fileType != ".jpeg") {
+    return res
+      .status(403)
+      .json({ success: false, message: "Image format invalid" });
+  }
+
+  fileType = fileType.substring(1, fileType.length);
+
+  const fileName = uuid.v4();
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName + "." + fileType,
+    Expires: 60 * 60,
+    ContentType: "image/" + fileType,
+    ACL: "public-read",
+  };
+
+  s3.getSignedUrl("putObject", s3Params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      success: true,
+      message: "Url generated",
+      uploadUrl: data,
+      downloadUrl:
+        `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}` + "." + fileType,
+    };
+    return res.status(201).json(returnData);
+  });
+};
+
+app.post("/generatePresignedUrl", (req, res) => getPresignedUrl(req, res));
